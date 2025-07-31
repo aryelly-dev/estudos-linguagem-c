819fa4b3fa98c498e3edcbcb920af68a28905fc0
Aryelly <aryellym@icloud.com>
2025-07-31 11:15:34 -0300

    add proced_welcome.c file
    [pt-BR]
    implementa função displayWelcome para exibir mensagem de boas-vindas.
    o programa chama a função displayWelcome, que imprime uma mensagem de saudação na tela. exemplo básico do uso de procedimentos (funções sem parâmetros e sem retorno) em C.
    
    [en]
    implements the displayWelcome function to show a welcome message.
    the program calls the displayWelcome function, which prints a greeting message on the screen. basic example of using procedures (functions without parameters and return values) in C.

---
+++ proced_welcome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Função que exibe uma mensagem de Boas-Vindas / Function that displays a Welcome message
+void displayWelcome() {
+    printf("\nWelcome to my program!\n\n");
+}
+
+int main(void) {
+    displayWelcome();
+
+    return 0;
+}
